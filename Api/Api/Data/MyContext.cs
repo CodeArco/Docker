39b893309653575577df223c71eed68907bd1858
codeArco <contacto@codearco.net>
2024-08-13 15:41:46 +0200

    no message

---
+++ Api/Api/Data/MyContext.cs
@@ -0,0 +1,15 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
